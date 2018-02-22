8040e0e5010a19de8ce88e72d4d5c46cfc07ffb2
Nina Klemencic <nina4klemencic@gmail.com>
2018-02-21 16:29:15 -0800

    user routes

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
 const
+  dotenv = require('dotenv').load()
   express = require('express')
   app = express()
+  logger = require('morgan')
+  bodyParser = require('body-parser')
+  mongoose = require('mongoose')
+  MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/ibike'
   PORT = process.env.PORT || 3001
+  usersRoutes = require('./routes/users.js')
 
-  app.use(express.static(`${__dirname}/client/build`))
-  
-  app.get('/api', (req, res) => {
-    res.json({message: "api root."})
-  })
+// connect ot db:
+mongoose.connect(MONGODB_URI, (err) => {
+  console.log(err || 'Connected to MongoDB.')
+})
 
-  app.get('*', (req, res) => {
-    res.sendFile(`${__dirname}/client/builf/index.html`)
-  })
+app.use(express.static(`${__dirname}/client/build`))
+app.user(logger('dev'))
+app.use(bodyParser.json())
 
-  app.listen(PORT, (err) => {
-    console.log(err || 'Server running on port ${PORT}')
-  })
+app.get('/api', (req, res) => {
+  res.json({message: "api root."})
+})
+
+app.use('/api/users', usersRoutes)
+
+app.use('*', (req, res) => {
+  res.sendFile(`${__dirname}/client/builf/index.html`)
+})
+
+app.listen(PORT, (err) => {
+  console.log(err || 'Server running on port ${PORT}')
+})
