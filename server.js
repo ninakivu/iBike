528c9d5ddad3f02102aeb8dcfc9347737154cc4d
Nina Klemencic <nina4klemencic@gmail.com>
2018-02-21 16:01:31 -0800

    server setup

--- server.js
+++ server.js
@@ -2,3 +2,17 @@ const
   express = require('express')
   app = express()
   PORT = process.env.PORT || 3001
+
+  app.use(express.static(`${__dirname}/client/build`))
+  
+  app.get('/api', (req, res) => {
+    res.json({message: "api root."})
+  })
+
+  app.get('*', (req, res) => {
+    res.sendFile(`${__dirname}/client/builf/index.html`)
+  })
+
+  app.listen(PORT, (err) => {
+    console.log(err || 'Server running on port ${PORT}')
+  })
